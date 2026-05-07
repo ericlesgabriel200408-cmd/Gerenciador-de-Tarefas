ebf40ce2fb3909e26f444568f4b5b3a2dad0e854
Ericles <ericlesgabriel200408@gmail.com>
2026-05-07 11:11:08 -0300

    nova atualização

--- Gerente de Tarefas/dados.py
+++ Gerente de Tarefas/dados.py
@@ -1,4 +1,5 @@
 ## Dados usados no projeto
 
 tarefas = []            #lista: armazena todas tarefas
-fila_pedente =[]        #
+fila_pedente = []        #fila (FIFO): armazena as tarefas que estão pendentes
+pilha_concluida = []     #pilha (LIFO): armazenaas tarefas
