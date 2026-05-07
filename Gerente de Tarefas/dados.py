41d1d0a3bacb60d61c67fd93ea114e5d3dac3b96
Nathan <nathan.g.n.santos@gmail.com>
2026-05-07 11:19:02 -0300

    teste

--- Gerente de Tarefas/dados.py
+++ Gerente de Tarefas/dados.py
@@ -3,3 +3,5 @@
 tarefas = []            #lista: armazena todas tarefas
 fila_pedente = []        #fila (FIFO): armazena as tarefas que estão pendentes
 pilha_concluida = []     #pilha (LIFO): armazenaas tarefas
+
+#teste
